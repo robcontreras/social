3d616b031e6b2a9c8db90c3c12e1cfd736bdc1ba
Rob Contreras <robcontreras@esmitv.com>
2012-05-17 09:54:35 -0600

    Added User model with devise and homepage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,10 @@ Social::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  
+  # Mailer
+  
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
@@ -34,4 +38,8 @@ Social::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # Heroku + Rails 3.1
+  #config.assets.initialize_on_precompile = false
+  
 end
