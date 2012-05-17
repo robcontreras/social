7c4f9919c8dd55c01daf8bbea99e5d76cce3ff0e
Rob Contreras <robcontreras@esmitv.com>
2012-05-17 08:59:20 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Social::Application.config.secret_token = '7be0851eeac907034ac5d78c67ce93de0dfd8958ef7ec6a38883d033321316ab33cbe43cfee9fd591b68825aa703aad77b4738497f4a508958142cb765dfefb9'
